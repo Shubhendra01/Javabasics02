36a30f44c798875e9a8fe3937c4bc849d0ba9ba3
unknown <shubhhhh17@gmail.com>
2022-03-15 20:32:15 +0530

    Largest of 3 numbers is created

---
+++ src/EvenOrOdd.java
@@ -0,0 +1,13 @@
+public class EvenOrOdd {
+
+    public static void main(String[] args)
+    {
+        Integer a = 3;
+        if (a % 2 == 0)
+            System.out.println(a+" is a even number.");
+        else
+            System.out.println(a+" is a odd number.");
+
+
+    }
+}
